08cdafa51e91f5d30fe3a318bd2752624c2241ba
Javier Palomero <jmpalomeros@gmail.com>
2022-10-21 15:35:14 +0200

    signup

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -20,6 +20,22 @@ router.post("/signup", async (req,res,next)=>{
         }) 
         return;
     }
+    //requisitos contraseña: 1 mayuscula, 1 numero y un signo 
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm;
+    if (passwordRegex.test(password) === false) {
+    res.render("auth/signup.hbs", {
+      errorMessage: "Invalid password",
+    });
+    return; 
+    }
+    //requisitos email
+    const mailRegex = /[\w|.|-]*@\w*\.[\w|.]*/g;
+    if (mailRegex.test(email) === false) {
+    res.render("auth/signup.hbs", {
+      errorMessage: "Invalid email.",
+    });
+    return; 
+    }
 
 
 
