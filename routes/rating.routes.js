ed58defd02a607037b2964dbf27561e4f4d05105
Ranier Mujica <ranieremujica@gmail.com>
2022-10-25 23:08:42 +0200

    validacion BD rating completa

--- routes/rating.routes.js
+++ routes/rating.routes.js
@@ -27,28 +27,65 @@ router.get("/:id/new-rating", isLogged, async (req, res, next) => {
 });
 
 //POST "/rating/new-rating" => ruta para añadir la valoracion
+// router.post("/:id/new-rating", isLogged, async (req, res, next) => {
+//     const { id } = req.params; //es del restaurante
+//     const {rating, recomendedDish, commentary } = req.body;
+//     console.log(req.body)
+//     console.log("el id del usuario:",req.session.loggedUser._id)
+//     let newRating = {
+//     restaurant: id,
+//     user: req.session.loggedUser._id,
+//     rating,
+//     recomendedDish,
+//     commentary,
+//   };
+
+//   try {
+    
+//     await Rating.create(newRating);
+//     res.redirect("/restaurant");
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+
 router.post("/:id/new-rating", isLogged, async (req, res, next) => {
     const { id } = req.params; //es del restaurante
     const {rating, recomendedDish, commentary } = req.body;
     console.log(req.body)
     console.log("el id del usuario:",req.session.loggedUser._id)
-    let newRating = {
-    restaurant: id,
-    user: req.session.loggedUser._id,
-    rating,
-    recomendedDish,
-    commentary,
-  };
+
   try {
+
+    const foundUser = await Rating.findOne({user: req.session.loggedUser._id})
+    console.log("quiero saber que se pasa aqui", foundUser)
+    if (foundUser !== null) {
+      res.render("rating/new-rating.hbs", {
+        errorMessage: "Ya has valorado este restaurante"
+      })
+      return;
+    }
+    
+    let newRating = {
+      restaurant: id,
+      user: req.session.loggedUser._id,
+      rating,
+      recomendedDish,
+      commentary,
+    };
+
     await Rating.create(newRating);
+    console.log("¿esto que es?", newRating)
     res.redirect("/restaurant");
+
   } catch (err) {
     next(err);
   }
 });
 
-//RUTA READ
 
+//RUTA READ
 //GET "/rating/:id/ratings" => renderizar todas las valoraciones de un mismo restaurante
 router.get("/:id/ratings", isLogged, async (req, res, next)=>{
   const {id} = req.params;
